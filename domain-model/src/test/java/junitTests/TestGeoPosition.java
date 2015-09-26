5eb5f54bb4493958d2ca1ad9a652d1d199ca487b
FCDevman <fac7@njit.edu>
2015-09-27 01:52:20 +1200

    Add test for GeoPosition.lengthTo()

--- domain-model/src/test/java/junitTests/TestGeoPosition.java
+++ domain-model/src/test/java/junitTests/TestGeoPosition.java
@@ -1,4 +1,5 @@
 package junitTests;
+
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -17,7 +18,7 @@ public class TestGeoPosition {
 	@Before
 	public void before() {
 		// Create a new GeoPosition for each test
-		testGeoPosotion = new GeoPosition(5.55,5.55);
+		testGeoPosotion = new GeoPosition(5.55, 5.55);
 	}
 
 	@Test
@@ -27,6 +28,16 @@ public class TestGeoPosition {
 		assertTrue(asString.equals("(5.55,5.55)"));
 		GeoPosition fromString = new GeoPosition(asString);
 		assertTrue(testGeoPosotion.equals(fromString));
+	}
+
+	@Test
+	public void testLengthTo() {
+		// Make another GeoPosition
+		GeoPosition position2 = new GeoPosition(0.0, 0.0);
+		// Check that the length is the same each way
+		assertTrue(position2.lengthTo(testGeoPosotion) == testGeoPosotion.lengthTo(position2));
+		logger.info("length is: " + position2.lengthTo(testGeoPosotion));
+		assertTrue(position2.lengthTo(testGeoPosotion) == 871226);
 	}
 
 }
