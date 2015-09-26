dd27e7e158a49c582711f82327e9d66f93aefe2f
FCDevman <fac7@njit.edu>
2015-09-26 23:45:48 +1200

    Added String marshalers for Joda DateTimes

---
+++ domain-model/src/main/java/car/Pooler/jaxb/DateTimeAdapter.java
@@ -0,0 +1,32 @@
+package car.Pooler.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.joda.time.DateTime;
+
+/**
+ * JAXB XML adapter to convert between Joda DateTime instances and Strings.
+ * DateTime objects are marshalled as Strings, and unmarshalled back into 
+ * DateTime instances.
+ *
+ */
+public class DateTimeAdapter extends XmlAdapter<String, DateTime> {
+
+	@Override
+	public DateTime unmarshal(String dateTimeAsString) throws Exception {
+		if(dateTimeAsString == null) {
+			return null;
+		}
+		
+		return new DateTime(dateTimeAsString);
+	}
+
+	@Override
+	public String marshal(DateTime dateTime) throws Exception {
+		if(dateTime == null) {
+			return null;
+		}
+		
+		return dateTime.toString();
+	}
+}
