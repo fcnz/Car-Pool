3161f932b67ddf965e3d2a9a08e3f7acc851ce71
FCDevman <fac7@njit.edu>
2015-09-30 00:01:06 +1300

    Add Resource class, add dependencies and plugins to pom.xml

---
+++ domain-model/src/main/java/carPooler/services/CarPoolerResource.java
@@ -0,0 +1,38 @@
+package carPooler.services;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.log4j.Logger;
+
+import carPooler.domain.Traveler;
+
+/**
+ * Web service resource implementation. An instance of this class will handle
+ * all HTTP requests.
+ *
+ * @author Fraser
+ *
+ */
+public class CarPoolerResource {
+
+	/**
+	* Logger for this class
+	*/
+	private static final Logger logger = Logger.getLogger(CarPoolerResource.class);
+
+	private Map<Long, Traveler> _travelerDB;
+	private AtomicLong _idCounter;
+
+	public CarPoolerResource() {
+		// reloadDatabase();
+	}
+
+	public Response createTraveler(carPooler.dto.Traveler dtoTraveler) {
+		return null;
+
+	}
+
+}
