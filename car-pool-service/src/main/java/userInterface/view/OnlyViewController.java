c79f7d81b80734a255797b70f5b5a3b1b018f079
FCDevman <fac7@njit.edu>
2015-10-02 04:09:44 +1300

    Added basic GUI

---
+++ car-pool-service/src/main/java/userInterface/view/OnlyViewController.java
@@ -0,0 +1,113 @@
+package userInterface.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class OnlyViewController {
+
+	@FXML
+	private TextArea display;
+	@FXML
+	private Label mainLabel;
+
+	// Buttons
+	@FXML
+	private Button button1;
+	@FXML
+	private Button button2;
+	@FXML
+	private Button button3;
+	@FXML
+	private Button button4;
+	@FXML
+	private Button button5;
+	@FXML
+	private Button button6;
+	@FXML
+	private Button button7;
+	@FXML
+	private Button button8;
+	@FXML
+	private Button button9;
+	@FXML
+	private Button button10;
+	@FXML
+	private Button button11;
+
+	// Text Fields
+	@FXML
+	private TextField field1;
+	@FXML
+	private TextField field2;
+	@FXML
+	private TextField field3;
+	@FXML
+	private TextField field4;
+	@FXML
+	private TextField field5;
+	@FXML
+	private TextField field6;
+	@FXML
+	private TextField field7;
+	@FXML
+	private TextField field8;
+
+	@FXML
+	private void button1() {
+		String str = field1.getText();
+	}
+
+	@FXML
+	private void button2() {
+		String str = field2.getText();
+	}
+
+	@FXML
+	private void button3() {
+		String str = field3.getText();
+	}
+
+	@FXML
+	private void button4() {
+		String str = field4.getText();
+	}
+
+	@FXML
+	private void button5() {
+		String str = field5.getText();
+	}
+
+	@FXML
+	private void button6() {
+		String str = field6.getText();
+	}
+
+	@FXML
+	private void button7() {
+		String str = field7.getText();
+	}
+
+	@FXML
+	private void button8() {
+		String str = field8.getText();
+	}
+
+	@FXML
+	private void button9() {
+
+	}
+
+	@FXML
+	private void button10() {
+
+	}
+
+	@FXML
+	private void button11() {
+
+	}
+
+}
